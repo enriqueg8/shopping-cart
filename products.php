9964f0d59b0221798a49a0b35faa93384a31fff5
enriqueg8 <garcia.enrique0128@gmail.com>
2026-06-23 11:08:54 -0400

    Add files via upload

---
+++ products.php
@@ -0,0 +1,9 @@
+<?php
+$products = array();
+$products[0] = array(1, "Laptop", 1250.00, true);
+$products[1] = array(2, "Airpods", 199.99, true);
+$products[2] = array(3, "Controller", 299.99, true);
+$products[3] = array(4, "Monitor", 250.00, true);
+$products[4] = array(5, "Playstation", 599.99, true);
+?>
+
